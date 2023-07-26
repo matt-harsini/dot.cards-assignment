41de7d00294b3eb8d412b276c5f2858e32e8f99b
Matthew Kim <matthewkimcs@gmail.com>
2023-07-26 12:51:25 -0700

    css tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,61 +23,63 @@ export default async function Home() {
   const data = await getData();
 
   return (
-    <main className="grid grid-cols-1 h-full">
-      <nav className="h-[64px] border-b-[1px]">
+    <main className="flex flex-col h-full">
+      <nav className="h-[64px] border-b-[1px] p-4">
         <div className="max-w-7xl mx-auto flex items-center justify-between h-full">
           <Logo style="text-gray-900" />
           <Cart />
         </div>
       </nav>
-      <header className="w-full mt-8 max-w-7xl mx-auto hero-bg flex rounded-3xl p-10 px-16">
-        <div className="flex flex-col flex-1 items-start justify-around">
-          <div>
-            <h3 className="text-[#EC5E2A] font-bold text-5xl uppercase">
-              25% off
-            </h3>
-            <h1 className="text-6xl font-bold mt-4">Summer Sale</h1>
-            <p className="text-xl mt-8">
-              Discover our summer styles with discount
-            </p>
+      <div className="flex-1">
+        <header className="w-full mt-8 max-w-7xl mx-auto hero-bg flex rounded-3xl p-10 px-16">
+          <div className="flex flex-col flex-1 items-start justify-around">
+            <div>
+              <h3 className="text-[#EC5E2A] font-bold text-5xl uppercase">
+                25% off
+              </h3>
+              <h1 className="text-6xl font-bold mt-4">Summer Sale</h1>
+              <p className="text-xl mt-8">
+                Discover our summer styles with discount
+              </p>
+            </div>
+            <button className="justify-self-end flex gap-x-2 justify-center items-center black py-5 px-20 rounded-xl">
+              <span className="text-white font-semibold">Shop Now</span>
+              <svg
+                className="svg-arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M18.6875 12.7188L13.6875 17.7188C13.5 17.9062 13.25 18 13 18C12.7188 18 12.4688 17.9062 12.2812 17.7188C11.875 17.3438 11.875 16.6875 12.2812 16.3125L15.5625 13H6C5.4375 13 5 12.5625 5 12C5 11.4688 5.4375 11 6 11H15.5625L12.2812 7.71875C11.875 7.34375 11.875 6.6875 12.2812 6.3125C12.6562 5.90625 13.3125 5.90625 13.6875 6.3125L18.6875 11.3125C19.0938 11.6875 19.0938 12.3438 18.6875 12.7188Z"
+                  fill="white"
+                />
+              </svg>
+            </button>
           </div>
-          <button className="justify-self-end flex gap-x-2 justify-center items-center black py-5 px-20 rounded-xl">
-            <span className="text-white font-semibold">Shop Now</span>
-            <svg
-              className="svg-arrow"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                d="M18.6875 12.7188L13.6875 17.7188C13.5 17.9062 13.25 18 13 18C12.7188 18 12.4688 17.9062 12.2812 17.7188C11.875 17.3438 11.875 16.6875 12.2812 16.3125L15.5625 13H6C5.4375 13 5 12.5625 5 12C5 11.4688 5.4375 11 6 11H15.5625L12.2812 7.71875C11.875 7.34375 11.875 6.6875 12.2812 6.3125C12.6562 5.90625 13.3125 5.90625 13.6875 6.3125L18.6875 11.3125C19.0938 11.6875 19.0938 12.3438 18.6875 12.7188Z"
-                fill="white"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="flex-1 flex items-center justify-center">
-          <Image
-            src="/images/hero.png"
-            alt="Green shoe"
-            width={490}
-            height={321}
-          />
-        </div>
-      </header>
-      <section className="max-w-7xl mx-auto mt-8">
-        <h3 className="text-4xl font-bold tracking-tight">
-          Explore our latest drops
-        </h3>
-        <div className="flex gap-x-8 mt-8">
-          {data.map((product: Product) => {
-            return <Card key={product.id} {...product} />;
-          })}
-        </div>
-      </section>
-      <footer className="black w-full flex items-center justify-center justify-self-end">
+          <div className="flex-1 flex items-center justify-center">
+            <Image
+              src="/images/hero.png"
+              alt="Green shoe"
+              width={490}
+              height={321}
+            />
+          </div>
+        </header>
+        <section className="max-w-7xl mx-auto mt-16">
+          <h3 className="text-4xl font-bold tracking-tight">
+            Explore our latest drops
+          </h3>
+          <div className="flex gap-x-8 mt-8">
+            {data.map((product: Product) => {
+              return <Card key={product.id} {...product} />;
+            })}
+          </div>
+        </section>
+      </div>
+      <footer className="mt-24 black w-full p-4 flex items-center justify-center justify-self-end">
         <div className="max-w-7xl w-full mx-auto flex justify-between items-center">
           <Logo style="text-white" />
           <span className="text-white text-sm">
